eeb3f1a750434a7875a0c52b6f8788708127756a
Juan Barros <juanlyrabarros@gmail.com>
2017-11-10 23:23:07 -0300

    Estrutura básica criada

--- CPP/Calango.cpp
+++ CPP/Calango.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
 
+using namespace std;
+
+struct Animal {
+  int life = 0;
+  int hunger = 0;
+  int energy = 0;
+  int evolution = 0;
+  int bathroom = 0;
+  bool isSleep = false;
+};
+typedef struct Animal Animal ;
+
+int main(){
+    Animal animal;
+    cout << animal.hunger;
+    return 0;
+}
