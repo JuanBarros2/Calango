4742770e0b02f78924a2b60cfe56750cb858ad3f
FelipeMarinho97 <felipevm97@gmail.com>
2017-12-07 13:33:42 -0300

    decrease feed level by round

--- Haskell/Calango.hs
+++ Haskell/Calango.hs
@@ -6,14 +6,14 @@ data Animal = Animal { name :: String
                } deriving (Show)
 
 main = do
-  putStrLn ("Bem vindo!")
-  putStrLn ("")
+  putStrLn ("Bem vindo!\n")
+
   putStrLn ("Qual o nome do seu bichinho?")
   nomeBichinho <- getLine
   let novoAnimal = Animal { name = nomeBichinho, stomach = 75, energy = 100, turns = 1, isSleep = False }
   menu novoAnimal
-  putStrLn ("")
-  putStrLn "O programa foi encerrado"
+
+  putStrLn "\nO programa foi encerrado"
 
 menu :: Animal -> IO Animal
 menu animal = do
@@ -26,14 +26,16 @@ menu animal = do
  executeOption animal (read option::Int)
 
 executeOption :: Animal -> Int -> IO Animal
-executeOption animal 1 = feed animal
-executeOption animal 3 = sleep animal
-executeOption animal 4 = return animal
+executeOption animal 1 = feed ( decreaseByRound animal)
+executeOption animal 3 = sleep ( decreaseByRound animal)
+executeOption animal 4 = return ( decreaseByRound animal)
 executeOption animal _ = do
                            putStrLn("\nOpção Inválida! Tente novamente...")
                            putStr("\nPressione <Enter> para voltar ao menu...")
                            getChar
                            menu animal
+      where animal = (decreaseByRound animal)
+
 
 -- Ainda não há conceito de níveis, nem de experiência
 -- As interações ainda não verifica estados dos atributos (se atingiu limite superior ou se chegou abaixo de 0)
@@ -49,6 +51,14 @@ calcFeed Animal { name = n, stomach = s, energy = e, turns = t, isSleep = sleep 
   stomach = if(s >= 75) then 100 else (s + 25),
   energy = e,
   turns = t + 1,
+  isSleep = sleep}
+
+decreaseByRound :: Animal -> Animal
+decreaseByRound (Animal { name = n, stomach = s, energy = e, turns = t, isSleep = sleep}) = Animal {
+  name = n,
+  stomach = if(s <= 5) then 0 else (s - 5),
+  energy = e,
+  turns = (t + 1),
   isSleep = sleep}
 
 -- Função sleep única: Ainda não há o estado "dormindo"
