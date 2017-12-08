22af799338481712d4bebd02977767ee89afff40
Juan Barros <juanlyrabarros@gmail.com>
2017-12-08 14:33:00 -0300

    Aumentando energia

--- Haskell/Calango.hs
+++ Haskell/Calango.hs
@@ -44,12 +44,25 @@ decreaseByRound :: Animal -> Animal
 decreaseByRound (Animal { name = n, stomach = s, life = l, caress = c, energy = e, turns = t, isSleep = sleep}) = Animal {
   name = n,
   stomach = if(s <= 5) then 0 else (s - 5),
-  life = if(s <= 15) then (l - 20) else l,
+  life = if calculateLife l s e sleep <= 0 then 0 else calculateLife l s e sleep,
   caress = if(c <= 10) then 0 else (c - 10),
-  energy = e,
+  energy = if (sleep) then e + 20 else e - 5,
   turns = (t + 1),
-  isSleep = sleep}
+  isSleep = sleep
 
+}
+
+calculateLife:: Int -> Int -> Int -> Bool -> Int
+calculateLife life stomach energy sleep = life - (calculateLifeDescontByEnergy energy) - (calculateLifeDescontByStomach stomach) + (calculateLifeIncreaseBySleep sleep)
+
+calculateLifeDescontByStomach:: Int -> Int
+calculateLifeDescontByStomach stomach = if (stomach <= 15) then 20 else 0
+
+calculateLifeDescontByEnergy:: Int -> Int
+calculateLifeDescontByEnergy energy = if (energy <= 30) then 20 else 0
+
+calculateLifeIncreaseBySleep:: Bool -> Int
+calculateLifeIncreaseBySleep isSleep = if isSleep then 2 else 0
 -- Ainda não há conceito de níveis, nem de experiência
 -- As interações ainda não verifica estados dos atributos (se atingiu limite superior ou se chegou abaixo de 0)
 feed :: Animal -> IO Animal
@@ -63,11 +76,10 @@ calcFeed Animal { name = n, stomach = s, life = l, caress = c, energy = e, turns
   name = n,
   stomach = if(s >= 75) then 100 else (s + 25),
   life = l,
-  caress = if(c >= 95) then 100 else if (s < 95) then (c + 5) else c,
+  caress = if(c >= 95) then 100 else (c + 5),
   energy = e,
   turns = t + 1,
   isSleep = sleep}
-
 
 -- Função sleep única: Ainda não há o estado "dormindo"
 sleep :: Animal -> IO Animal
@@ -79,12 +91,12 @@ sleep animal = do
 calcSleep :: Animal -> Animal
 calcSleep (Animal { name = n, stomach = h, life = l, caress = c, energy = e, turns = t, isSleep = sleep}) = Animal {
   name = n,
-  stomach = (h - 20),
-  life = l,
+  stomach = h,
+  life = l + 2,
   caress = c,
-  energy = e ,
+  energy = if(e >= 80) then 100 else (e + 20),
   turns = (t + 1),
-  isSleep = sleep}
+  isSleep = True}
 
 
 toCaress :: Animal -> IO Animal
@@ -108,7 +120,8 @@ showStatus :: Animal -> String
 showStatus (Animal {name = n, stomach = h, life = l, caress = c, energy = e,
                turns = t, isSleep = sleep})
     | h <= 20 = "PERIGO EMINENTE!!! " ++ n ++ " está a com fome alta, alimente-o já!\n" ++ status
-    | c <= 20 = "CARÊNCIA CRÍTICA!! Parece que você é um verdadeito BRUTA-MONTES, cuide do seu calango seu(a) cabra da peste!!"
+    | c <= 20 = "CARÊNCIA CRÍTICA!! Parece que você é um verdadeito BRUTA-MONTES, cuide do seu calango seu(a) cabra da peste!!\n" ++ status
+    | e <= 30 = "SONO CRÍTICO!!! " ++ n ++ " precisa de um descanso ou não terá energia para escapar de predadores. Apague a luz!\n" ++ status
     | otherwise = status
     where status = "Nome: " ++ n ++ "\nVida: " ++ show l ++ " Estomago: " ++ show h ++ "% Carinho: " ++ show c ++ "% Energia: "
                       ++ show e ++ "%"
