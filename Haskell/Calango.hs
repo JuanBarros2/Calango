de9682c65ed8ad61e0494fc51bead10f92e86767
Juan Barros <juanlyrabarros@gmail.com>
2017-12-08 16:56:39 -0300

    Acendendo e apagando lampada

--- Haskell/Calango.hs
+++ Haskell/Calango.hs
@@ -13,31 +13,41 @@ main = do
   putStrLn ("Qual o nome do seu bichinho?")
   nomeBichinho <- getLine
   let novoAnimal = Animal { name = nomeBichinho, stomach = 75, life = 100, caress = 100, energy = 100, turns = 1, isSleep = False }
-  menu novoAnimal
+  menu novoAnimal False
 
   putStrLn "\nO programa foi encerrado"
 
-menu :: Animal -> IO Animal
-menu animal = do
+menu :: Animal -> Bool -> IO Animal
+menu animal False = do
  putStrLn ("")
  putStrLn ("1 - Alimentar")
  putStrLn ("2 - Ir ao Banheiro")
- putStrLn ("3 - Dormir")
+ putStrLn ("3 - Desligar a luz")
  putStrLn ("4 - Acariciar")
  putStrLn ("0 - Sair")
  option <- getLine
- executeOption animal (read option::Int)
+ executeOption animal (read option::Int) False
 
-executeOption :: Animal -> Int -> IO Animal
-executeOption animal 1 = feed ( decreaseByRound animal)
-executeOption animal 3 = sleep ( decreaseByRound animal)
-executeOption animal 4 = toCaress (decreaseByRound animal)
-executeOption animal 0 = return ( decreaseByRound animal)
-executeOption animal _ = do
+menu animal True = do
+  putStrLn ("")
+  putStrLn ("1 - Ligar luz")
+  putStrLn ("2 - Continuar dormindo")
+  putStrLn ("0 - Sair")
+  option <- getLine
+  executeOption animal (read option::Int) True
+
+executeOption :: Animal -> Int -> Bool -> IO Animal
+executeOption animal 1 False = feed (decreaseByRound animal)
+executeOption animal 1 True = wakeUp (decreaseByRound animal)
+executeOption animal 2 True = sleep (decreaseByRound animal)
+executeOption animal 3 False = sleep (decreaseByRound animal)
+executeOption animal 4 False = toCaress (decreaseByRound animal)
+executeOption animal 0 anyValue = return (decreaseByRound animal)
+executeOption animal _ anyValue = do
                            putStrLn("\nOpção Inválida! Tente novamente...")
                            putStr("\nPressione <Enter> para voltar ao menu...")
                            getChar
-                           menu animal
+                           menu animal anyValue
 
 
 decreaseByRound :: Animal -> Animal
@@ -49,7 +59,6 @@ decreaseByRound (Animal { name = n, stomach = s, life = l, caress = c, energy = 
   energy = if (sleep) then e + 20 else e - 5,
   turns = (t + 1),
   isSleep = sleep
-
 }
 
 calculateLife:: Int -> Int -> Int -> Bool -> Int
@@ -69,7 +78,7 @@ feed :: Animal -> IO Animal
 feed animal = do
               let updatedAnimal = calcFeed animal
               putStrLn (showStatus updatedAnimal)
-              menu updatedAnimal
+              menu updatedAnimal False
 
 calcFeed :: Animal -> Animal
 calcFeed Animal { name = n, stomach = s, life = l, caress = c, energy = e, turns = t, isSleep = sleep } = Animal {
@@ -81,12 +90,28 @@ calcFeed Animal { name = n, stomach = s, life = l, caress = c, energy = e, turns
   turns = t + 1,
   isSleep = sleep}
 
+wakeUp :: Animal -> IO Animal
+wakeUp animal = do
+    let updatedAnimal = calcWakeUp animal
+    putStrLn (showStatus updatedAnimal)
+    menu updatedAnimal False
+
+calcWakeUp :: Animal -> Animal
+calcWakeUp Animal { name = n, stomach = s, life = l, caress = c, energy = e, turns = t, isSleep = sleep } = Animal {
+    name = n,
+    stomach = s,
+    life = l,
+    caress = if(c >= 95) then 100 else (c + 5),
+    energy = e,
+    turns = t + 1,
+    isSleep = False}
+
 -- Função sleep única: Ainda não há o estado "dormindo"
 sleep :: Animal -> IO Animal
 sleep animal = do
                let updatedAnimal = calcSleep animal
                putStrLn (showStatus updatedAnimal)
-               menu updatedAnimal
+               menu updatedAnimal True
 
 calcSleep :: Animal -> Animal
 calcSleep (Animal { name = n, stomach = h, life = l, caress = c, energy = e, turns = t, isSleep = sleep}) = Animal {
@@ -96,14 +121,14 @@ calcSleep (Animal { name = n, stomach = h, life = l, caress = c, energy = e, tur
   caress = c,
   energy = if(e >= 80) then 100 else (e + 20),
   turns = (t + 1),
-  isSleep = True}
+  isSleep = True }
 
 
 toCaress :: Animal -> IO Animal
 toCaress animal = do
                   let updatedAnimal = calcToCaress animal
                   putStrLn (showStatus updatedAnimal)
-                  menu updatedAnimal
+                  menu updatedAnimal False
 
 calcToCaress :: Animal -> Animal
 calcToCaress (Animal { name = n, stomach = h, life = l, caress = c, energy = e, turns = t, isSleep = sleep}) = Animal {
