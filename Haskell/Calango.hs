c1d58f727916209600720713b081e987d38f84ba
José Renan <joserenansl99@gmail.com>
2017-12-08 19:57:56 -0300

    Corrigindo problemas de merge que ignoraram commits passados

--- Haskell/Calango.hs
+++ Haskell/Calango.hs
@@ -1,27 +1,28 @@
 
-data Animal = Animal { name :: String
-               , stomach :: Int
-               , energy :: Int
-               , life :: Int
-               , cleanness :: Int
-               , caress :: Int
-               , turns :: Int
-               , isSleep :: Bool
-               } deriving (Show)
+data Animal = Animal {
+    name :: String,
+    stomach :: Int,
+    energy :: Int,
+    life :: Int,
+    cleanness :: Int,
+    caress :: Int,
+    turns :: Int,
+    isSleep :: Bool
+  } deriving (Show)
 
 main = do
-    putStrLn (" =========================================== \n")
-    putStrLn ("|                                           |\n")
-    putStrLn ("|                CALANGO                    |\n")
-    putStrLn ("|                                           |\n")
-    putStrLn ("|           1- Começar jogo                 |\n")
-    putStrLn ("|           2- Instruções                   |\n")
-    putStrLn ("|           3- Sair                         |\n")
-    putStrLn ("|                                           |\n")
-    putStrLn (" =========================================== \n")
-    opcao <- getLine
-    selectStart (read opcao)
-    if (read opcao) == 3 then putStr("Fim") else main
+  putStrLn (" =========================================== \n")
+  putStrLn ("|                                           |\n")
+  putStrLn ("|                CALANGO                    |\n")
+  putStrLn ("|                                           |\n")
+  putStrLn ("|           1- Começar jogo                 |\n")
+  putStrLn ("|           2- Instruções                   |\n")
+  putStrLn ("|           3- Sair                         |\n")
+  putStrLn ("|                                           |\n")
+  putStrLn (" =========================================== \n")
+  opcao <- getLine
+  selectStart (read opcao)
+  if (read opcao) == 3 then putStr("Fim") else main
 
 selectStart :: Int -> IO ()
 selectStart 1 = startGame
@@ -31,7 +32,8 @@ selectStart _ = putStr ""
 
 startGame :: IO ()
 startGame = do
-  putStrLn ("Qual o nome do seu bichinho?")
+  showDivisor
+  putStrLn ("\nQual o nome do seu bichinho?")
   nomeBichinho <- getLine
   let novoAnimal = Animal { name = nomeBichinho, stomach = 75, life = 100, caress = 100, energy = 100, turns = 1, isSleep = False, cleanness = 100 }
   menu novoAnimal False
@@ -43,16 +45,19 @@ instructions = do
 
 menu :: Animal -> Bool -> IO Animal
 menu animal False = do
- putStrLn ("")
- putStrLn ("1 - Alimentar")
- putStrLn ("2 - Limpar o ambiente")
- putStrLn ("3 - Desligar a luz")
- putStrLn ("4 - Acariciar/Brincar")
- putStrLn ("0 - Sair")
- option <- getLine
- executeOption animal (read option::Int) False
+  showDivisor
+  putStrLn (showStatus animal)
+  putStrLn ("")
+  putStrLn ("1 - Alimentar")
+  putStrLn ("2 - Limpar o ambiente")
+  putStrLn ("3 - Desligar a luz")
+  putStrLn ("4 - Acariciar/Brincar")
+  putStrLn ("0 - Sair")
+  option <- getLine
+  executeOption animal (read option::Int) False
 
 menu animal True = do
+  putStrLn (showStatus animal)
   putStrLn ("")
   putStrLn ("1 - Ligar luz")
   putStrLn ("2 - Continuar dormindo")
@@ -69,19 +74,26 @@ executeOption animal 3 False = sleep (decreaseByRound animal)
 executeOption animal 4 False = toCaress (decreaseByRound animal)
 executeOption animal 0 anyValue = return (decreaseByRound animal)
 executeOption animal _ anyValue = do
-                           putStrLn("\nOpção Inválida! Tente novamente...")
-                           putStr("\nPressione <Enter> para voltar ao menu...")
-                           getChar
-                           menu animal anyValue
+  putStrLn("\nOpção Inválida! Tente novamente...")
+  putStr("\nPressione <Enter> para voltar ao menu...")
+  getChar
+  menu animal anyValue
 
+
+-- As funções de inscrease e decrease do sleep calculam a energia (por round) de forma que não ultrapasse o range [0, 100] --
+increaseSleep :: Int -> Int
+increaseSleep energy = if (energy >= 80) then 100 else (energy + 20)
+
+decreaseSleep :: Int -> Int
+decreaseSleep energy = if (energy <= 5) then 0 else (energy - 5)
 
 decreaseByRound :: Animal -> Animal
 decreaseByRound (Animal { name = n, stomach = s, life = l, caress = c, energy = e, turns = t, isSleep = sleep, cleanness = cl}) = Animal {
   name = n,
   stomach = if(s <= 5) then 0 else (s - 5),
-  life = if calculateLife l s e cl sleep <= 0 then 0 else calculateLife l s e cl sleep,
+  life = if calculateLife l s e cl sleep <= 0 then 0 else if (calculateLife l s e cl sleep >= 100) then 100 else calculateLife l s e cl sleep,
   caress = if(c <= 10) then 0 else (c - 10),
-  energy = if (sleep) then e + 20 else e - 5,
+  energy = if (sleep) then increaseSleep e else decreaseSleep e,
   turns = (t + 1),
   isSleep = sleep,
   cleanness = cl
@@ -91,7 +103,7 @@ calculateLife:: Int -> Int -> Int -> Int-> Bool -> Int
 calculateLife life stomach energy cleanness sleep = life - (calculateLifeDescontByEnergy energy) - (calculateLifeDescontByStomach stomach) - (calculateLifeDescontByCleanness cleanness) + (calculateLifeIncreaseBySleep sleep)
 
 calculateLifeDescontByCleanness:: Int -> Int
-calculateLifeDescontByCleanness cleanness = if (cleanness <= 80) then 20 else 0
+calculateLifeDescontByCleanness cleanness = if (cleanness <= 70) then 20 else 0
 
 calculateLifeDescontByStomach:: Int -> Int
 calculateLifeDescontByStomach stomach = if (stomach <= 15) then 20 else 0
@@ -101,13 +113,14 @@ calculateLifeDescontByEnergy energy = if (energy <= 30) then 20 else 0
 
 calculateLifeIncreaseBySleep:: Bool -> Int
 calculateLifeIncreaseBySleep isSleep = if isSleep then 2 else 0
+
 -- Ainda não há conceito de níveis, nem de experiência
 -- As interações ainda não verifica estados dos atributos (se atingiu limite superior ou se chegou abaixo de 0)
+
 feed :: Animal -> IO Animal
 feed animal = do
-              let updatedAnimal = calcFeed animal
-              putStrLn (showStatus updatedAnimal)
-              menu updatedAnimal False
+  let updatedAnimal = calcFeed animal
+  menu updatedAnimal False
 
 calcFeed :: Animal -> Animal
 calcFeed Animal { name = n, stomach = s, life = l, caress = c, energy = e, turns = t, isSleep = sleep, cleanness = cl } = Animal {
@@ -117,14 +130,13 @@ calcFeed Animal { name = n, stomach = s, life = l, caress = c, energy = e, turns
   caress = if(c >= 95) then 100 else (c + 5),
   energy = e,
   turns = t + 1,
-  cleanness = if (cl <= 5) then 0 else (cl - 5),
+  cleanness = if (cl <= 10) then 0 else (cl - 10),
   isSleep = sleep}
 
 wakeUp :: Animal -> IO Animal
 wakeUp animal = do
-    let updatedAnimal = calcWakeUp animal
-    putStrLn (showStatus updatedAnimal)
-    menu updatedAnimal False
+  let updatedAnimal = calcWakeUp animal
+  menu updatedAnimal False
 
 calcWakeUp :: Animal -> Animal
 calcWakeUp Animal { name = n, stomach = s, life = l, caress = c, energy = e, turns = t, isSleep = sleep, cleanness = cl} = Animal {
@@ -135,20 +147,20 @@ calcWakeUp Animal { name = n, stomach = s, life = l, caress = c, energy = e, tur
     energy = e,
     turns = t + 1,
     cleanness = cl,
-    isSleep = False}
+    isSleep = False
+  }
 
 -- Função sleep única: Ainda não há o estado "dormindo"
 sleep :: Animal -> IO Animal
 sleep animal = do
-               let updatedAnimal = calcSleep animal
-               putStrLn (showStatus updatedAnimal)
-               menu updatedAnimal True
+  let updatedAnimal = calcSleep animal
+  menu updatedAnimal True
 
 calcSleep :: Animal -> Animal
 calcSleep (Animal { name = n, stomach = h, life = l, caress = c, energy = e, turns = t, isSleep = sleep, cleanness = cl}) = Animal {
   name = n,
   stomach = h,
-  life = l + 2,
+  life = if (l > 98) then 100 else (l + 2),
   caress = c,
   energy = if(e >= 80) then 100 else (e + 20),
   turns = (t + 1),
@@ -158,26 +170,25 @@ calcSleep (Animal { name = n, stomach = h, life = l, caress = c, energy = e, tur
 
 toCaress :: Animal -> IO Animal
 toCaress animal = do
-                  let updatedAnimal = calcToCaress animal
-                  putStrLn (showStatus updatedAnimal)
-                  menu updatedAnimal False
+  let updatedAnimal = calcToCaress animal
+  menu updatedAnimal False
 
 calcToCaress :: Animal -> Animal
 calcToCaress (Animal { name = n, stomach = h, life = l, caress = c, energy = e, turns = t, isSleep = sleep, cleanness = cl}) = Animal {
-  name = n,
-  stomach = h,
-  life = l,
-  caress = c + 40,
-  energy = e ,
-  turns = (t + 1),
-  isSleep = sleep,
-  cleanness = cl}
+    name = n,
+    stomach = h,
+    life = l,
+    caress = if (c >= 60) then 100 else (c + 40),
+    energy = e ,
+    turns = (t + 1),
+    isSleep = sleep,
+    cleanness = cl
+  }
 
 toClean :: Animal -> IO Animal
 toClean animal = do
-                  let updatedAnimal = calcToClean animal
-                  putStrLn (showStatus updatedAnimal)
-                  menu updatedAnimal False
+  let updatedAnimal = calcToClean animal
+  menu updatedAnimal False
 
 calcToClean :: Animal -> Animal
 calcToClean (Animal { name = n, stomach = h, life = l, caress = c, energy = e, turns = t, isSleep = sleep, cleanness = cl}) = Animal {
@@ -194,14 +205,17 @@ calcToClean (Animal { name = n, stomach = h, life = l, caress = c, energy = e, t
 showStatus :: Animal -> String
 showStatus (Animal {name = n, stomach = h, life = l, caress = c, energy = e,
                turns = t, isSleep = sleep, cleanness = cl})
-    | h <= 20 = "PERIGO EMINENTE!!! " ++ n ++ " está a com fome alta, alimente-o já!\n" ++ status
-    | c <= 20 = "CARÊNCIA CRÍTICA!! Parece que você é um verdadeito BRUTA-MONTES, cuide do seu calango seu(a) cabra da peste!!\n" ++ status
-    | e <= 30 = "SONO CRÍTICO!!! " ++ n ++ " precisa de um descanso ou não terá energia para escapar de predadores. Apague a luz!\n" ++ status
-    | cl <= 80 = "LIMPE O AMBIENTE!!! " ++ n ++ " está cambaleando no lixo!\n" ++ status
-    | otherwise = status
-    where status = "Nome: " ++ n
-                      ++ "\nVida: " ++ show l
-                      ++ "% Estomago: " ++ show h
-                      ++ "% Carinho: " ++ show c
-                      ++ "% Energia: " ++ show e
-                      ++ "% Limpeza do ambiente " ++ show  cl ++ "%"
+  | h <= 20 = "\nPERIGO EMINENTE!!! " ++ n ++ " está a com fome alta, alimente-o já!\n" ++ status
+  | c <= 20 = "\nCARÊNCIA CRÍTICA!! Parece que você é um verdadeito BRUTA-MONTES, cuide do seu calango seu(a) cabra da peste!!\n" ++ status
+  | e <= 30 = "\nSONO CRÍTICO!!! " ++ n ++ " precisa de um descanso ou não terá energia para escapar de predadores. Apague a luz!\n" ++ status
+  | cl <= 70 = "\nQUE SUJEIRA!!! " ++ n ++ " está cambaleando no lixo!\n" ++ status
+  | otherwise = status
+  where status = "\nNome: " ++ n
+              ++ "\nVida: " ++ show l
+              ++ "%, Estomago: " ++ show h
+              ++ "%, Carinho: " ++ show c
+              ++ "%,\nEnergia: " ++ show e
+              ++ "%, Limpeza do ambiente " ++ show  cl ++ "%"
+
+showDivisor = do
+  putStrLn ("\n=========================================================")
