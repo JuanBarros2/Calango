8f2c1f7dd536a30b77f2ba6fb0f5dc5c105383be
Juan Barros <juanlyrabarros@gmail.com>
2017-12-10 11:37:29 -0300

    Finalizando jogo ao chegar em 0% de vida #13

--- Haskell/Calango.hs
+++ Haskell/Calango.hs
@@ -11,22 +11,22 @@ data Animal = Animal {
   } deriving (Show)
 
 main = do
-  putStrLn (" =========================================== \n")
-  putStrLn ("|                                           |\n")
-  putStrLn ("|                CALANGO                    |\n")
-  putStrLn ("|                                           |\n")
-  putStrLn ("|           1- Começar jogo                 |\n")
-  putStrLn ("|           2- Instruções                   |\n")
-  putStrLn ("|           3- Sair                         |\n")
-  putStrLn ("|                                           |\n")
-  putStrLn (" =========================================== \n")
+  putStrLn (" ================================= ")
+  putStrLn ("|                                 |")
+  putStrLn ("|       CALANGO                   |")
+  putStrLn ("|                                 |")
+  putStrLn ("|       1- Começar jogo           |")
+  putStrLn ("|       2- Instruções             |")
+  putStrLn ("|       3- Sair                   |")
+  putStrLn ("|                                 |")
+  putStrLn (" ================================= ")
   opcao <- getLine
   selectStart (read opcao)
   if (read opcao) == 3 then putStr("Fim") else main
 
 selectStart :: Int -> IO ()
 selectStart 1 = startGame
-selectStart 2 = putStrLn "Aqui ficam as instruções para jogar"
+selectStart 2 = instructions
 selectStart _ = putStr ""
 
 
@@ -37,13 +37,37 @@ startGame = do
   nomeBichinho <- getLine
   let novoAnimal = Animal { name = nomeBichinho, stomach = 75, life = 100, caress = 100, energy = 100, turns = 1, isSleep = False, cleanness = 100 }
   menu novoAnimal False
+  showDivisor
   putStrLn (("\n" ++ nomeBichinho) ++ " morreu :/")
+  putStrLn "Pressione ENTER para continuar"
+  getChar
+  putStrLn ""
 
 instructions :: IO ()
 instructions = do
-  putStrLn "O seu bichinho virtual precisa de ajuda para sobreviver "
+  showDivisor
+  putStrLn ""
+  putStr "O seu bichinho virtual precisa de ajuda para sobreviver. Você precisa manter seus níveis "
+  putStr "sempre no máximo. Ao inicilizar o jogo, você verá marcadores que vão de 0% a 100%. Cada "
+  putStr "marcador é afetado pela ação que você toma. Para entender como as ações influenciam, segue "
+  putStr "abaixo uma tabela de Ações e Reações para cada instrução:"
+  tableInstruction
+
+tableInstruction :: IO ()
+tableInstruction = do
+  putStrLn "\n"
+  putStrLn "Alimentar = +25% de Estômago; +5% de Carinho; -10% de Limpeza;"
+  putStrLn "Limpar o ambiente = +45% de Limpeza; +5% de Carinho;"
+  putStrLn "Desligar a luz/Continuar dormindo = +20% de Energia; +2% de Vida;"
+  putStrLn "Brincar = +40% de Carinho;"
+  putStrLn "Aplicar injeção = +50% de Vida; -30% de Carinho;"
+  putStrLn ""
+  putStrLn "Pressione ENTER para continuar"
+  cm <- getLine
+  showDivisor
 
 menu :: Animal -> Bool -> IO Animal
+menu Animal { life = 0 } anyValue = return Animal {name = "", stomach = 0, life = 0, caress = 0, energy = 0, turns = 0, isSleep = False, cleanness = 0}
 menu animal False = do
   showDivisor
   putStrLn (showStatus animal)
@@ -51,7 +75,8 @@ menu animal False = do
   putStrLn ("1 - Alimentar")
   putStrLn ("2 - Limpar o ambiente")
   putStrLn ("3 - Desligar a luz")
-  putStrLn ("4 - Acariciar/Brincar")
+  putStrLn ("4 - Brincar")
+  putStrLn ("5 - Aplicar injeção")
   putStrLn ("0 - Sair")
   option <- getLine
   executeOption animal (read option::Int) False
@@ -72,13 +97,29 @@ executeOption animal 2 True = sleep (decreaseByRound animal)
 executeOption animal 2 False = toClean (decreaseByRound animal)
 executeOption animal 3 False = sleep (decreaseByRound animal)
 executeOption animal 4 False = toCaress (decreaseByRound animal)
-executeOption animal 0 anyValue = return (decreaseByRound animal)
+executeOption animal 5 False = heal(decreaseByRound animal)
+executeOption animal 0 anyValue = return animal
 executeOption animal _ anyValue = do
   putStrLn("\nOpção Inválida! Tente novamente...")
   putStr("\nPressione <Enter> para voltar ao menu...")
   getChar
   menu animal anyValue
 
+heal :: Animal -> IO Animal
+heal animal = do
+  let updatedAnimal = calcHeal animal
+  menu updatedAnimal False
+
+calcHeal :: Animal -> Animal
+calcHeal Animal { name = n, stomach = s, life = l, caress = c, energy = e, turns = t, isSleep = sleep, cleanness = cl } = Animal {
+  name = n,
+  stomach = s,
+  life = if(l >= 50) then 100 else (l + 50),
+  caress = if(c <= 30) then 0 else (c - 30),
+  energy = e,
+  turns = t + 1,
+  cleanness = cl,
+  isSleep = sleep}
 
 -- As funções de inscrease e decrease do sleep calculam a energia (por round) de forma que não ultrapasse o range [0, 100] --
 increaseSleep :: Int -> Int
@@ -195,7 +236,7 @@ calcToClean (Animal { name = n, stomach = h, life = l, caress = c, energy = e, t
   name = n,
   stomach = h,
   life = l,
-  caress = c,
+  caress = if(c >= 95) then 100 else (c + 5),
   energy = e ,
   turns = (t + 1),
   isSleep = sleep,
