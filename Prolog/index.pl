69a57449aae3478fa7ba2a35e3dc8ed9575a9a34
Felipe Marinho <felipevm97@gmail.com>
2018-03-08 03:44:43 -0300

    implementado novas funcs

--- Prolog/index.pl
+++ Prolog/index.pl
@@ -21,73 +21,127 @@ startGame:-
 
 mainCircle(NAME, _, _, _, _, 0, _):- write(NAME), write(" morreu").
 mainCircle(NAME, FOOD, CLEAR, ENERGY, LOVE, LIFE, true):-
-    printInfo(NAME, FOOD, CLEAR, ENERGY, LOVE),
+    printInfo(NAME, FOOD, CLEAR, ENERGY, LOVE, LIFE),
     getActionSleep(ACTION),
     actionSleep(NAME, FOOD, CLEAR, ENERGY, LOVE, LIFE, true, ACTION).
 mainCircle(NAME, FOOD, CLEAR, ENERGY, LOVE, LIFE, SLEEP):-
-    printInfo(NAME, FOOD, CLEAR, ENERGY, LOVE),
+    printInfo(NAME, FOOD, CLEAR, ENERGY, LOVE, LIFE),
     getAction(ACTION),
-    actionGame(FOOD, CLEAR, ENERGY, LOVE, SLEEP, NFOOD, NCLEAR, NENERGY, NLOVE, NSLEEP, ACTION),
-    decreaseTurn(NCLEAR, NFOOD, NENERGY, NLOVE, DCLEAR, DFOOD, DENERGY, DLOVE),
-    mainCircle(NAME, DCLEAR, DFOOD, DENERGY, DLOVE, LIFE, NSLEEP).
+    actionGame(FOOD, CLEAR, ENERGY, LOVE, SLEEP, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, NSLEEP, NLIFE, ACTION),
+    decreaseTurn(NFOOD, NCLEAR, NENERGY, NLOVE, NLIFE, DFOOD, DCLEAR, DENERGY, DLOVE, DLIFE),
+    mainCircle(NAME, DCLEAR, DFOOD, DENERGY, DLOVE, DLIFE, NSLEEP).
 
-getAction(ACTION):- writeln("1- Alimentar"),
+getAction(ACTION):-
+    writeln("1- Alimentar"),
     writeln("2- Limpar"),
     writeln("3- Dormir"),
     writeln("4- Dar carinho"),
     read(ACTION).
 
-actionGame(FOOD, CLEAR, ENERGY, LOVE, _, NFOOD, NCLEAR, NENERGY, NLOVE, NSLEEP, 1):-
+actionGame(FOOD, CLEAR, ENERGY, LOVE, _, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, NSLEEP, LIFE, 1):- % alimentar
     NFOOD is FOOD + 25,
     NCLEAR is CLEAR,
     NENERGY is ENERGY,
     NLOVE is LOVE + 5,
     NSLEEP=false.
-actionGame(FOOD, _, ENERGY, LOVE, _, NFOOD, NCLEAR, NENERGY, NLOVE, NSLEEP, 2):-
+actionGame(FOOD, _, ENERGY, LOVE, _, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, NSLEEP, LIFE, 2):- % limpar
     NFOOD is FOOD,
     NCLEAR is 100,
     NENERGY is ENERGY,
     NLOVE is LOVE + 5,
     NSLEEP=false.
-actionGame(FOOD, CLEAR, ENERGY, LOVE, _, NFOOD, NCLEAR, NENERGY, NLOVE, NSLEEP, 4):-
+actionGame(FOOD, CLEAR, ENERGY, LOVE, _, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, NSLEEP, NLIFE, 3):- % dormir
+    NFOOD is FOOD,
+    NCLEAR is CLEAR,
+    NENERGY is ENERGY + 25,
+    NLOVE is LOVE,
+    NLIFE is LIFE + 2,
+    NSLEEP=true.
+actionGame(FOOD, CLEAR, ENERGY, LOVE, _, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, NSLEEP, LIFE, 4):- % dar carinho
     NFOOD is FOOD,
     NCLEAR is CLEAR,
     NENERGY is ENERGY,
     NLOVE is LOVE + 40,
     NSLEEP=false.
-actionGame(FOOD, CLEAR, ENERGY, LOVE, _, NFOOD, NCLEAR, NENERGY, NLOVE, NSLEEP, 3):-
-    NFOOD is FOOD,
-    NCLEAR is CLEAR,
-    NENERGY is ENERGY + 20,
-    NLOVE is LOVE,
-    NSLEEP=true.
 
-actionSleep(NAME, FOOD, CLEAR, ENERGY, LOVE, LIFE, SLEEP, 2):-
-    actionGame(FOOD, CLEAR, ENERGY, LOVE, SLEEP, NFOOD, NCLEAR, NENERGY, NLOVE, NSLEEP, 3),
-    decreaseTurn(NCLEAR, NFOOD, NENERGY, NLOVE, DCLEAR, DFOOD, DENERGY, DLOVE),
-    mainCircle(NAME, DCLEAR, DFOOD, DENERGY, DLOVE, LIFE, NSLEEP).
-actionSleep(NAME, FOOD, CLEAR, ENERGY, LOVE, LIFE, _, 1):-
+actionSleep(NAME, FOOD, CLEAR, ENERGY, LOVE, LIFE, SLEEP, 2):- % continuar dormindo
+    actionGame(FOOD, CLEAR, ENERGY, LOVE, SLEEP, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, NSLEEP, NLIFE, 3),
+    decreaseTurn(NFOOD, NCLEAR, NENERGY, NLOVE, NLIFE, DFOOD, DCLEAR, DENERGY, DLOVE, DLIFE),
+    mainCircle(NAME, DCLEAR, DFOOD, DENERGY, DLOVE, DLIFE, NSLEEP).
+actionSleep(NAME, FOOD, CLEAR, ENERGY, LOVE, LIFE, _, 1):- % acordar
     NFOOD is FOOD,
     NCLEAR is CLEAR,
     NENERGY is ENERGY,
     NLOVE is LOVE + 5,
     NSLEEP=false,
-    decreaseTurn(NCLEAR, NFOOD, NENERGY, NLOVE, DCLEAR, DFOOD, DENERGY, DLOVE),
-    mainCircle(NAME, DCLEAR, DFOOD, DENERGY, DLOVE, LIFE, NSLEEP).
+    decreaseTurn(NFOOD, NCLEAR, NENERGY, NLOVE, LIFE, DFOOD, DCLEAR, DENERGY, DLOVE, DLIFE),
+    mainCircle(NAME, DCLEAR, DFOOD, DENERGY, DLOVE, DLIFE, NSLEEP).
 
 getActionSleep(ACTION):-
     writeln("1- Acordar"),
     writeln("2- Continuar dormindo"),
     read(ACTION).
 
-decreaseTurn(FOOD, CLEAR, ENERGY, LOVE, NFOOD, NCLEAR, NENERGY, NLOVE):-
+
+decreaseTurn(FOOD, CLEAR, ENERGY, LOVE, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, NLIFE):-
     NFOOD is FOOD - 5,
     NCLEAR is CLEAR - 10,
-    NENERGY is ENERGY,
-    NLOVE is LOVE.
+    NENERGY is ENERGY - 5,
+    NLOVE is LOVE - 10,
+    NLIFE is LIFE - 20,
+    criticalLove(NLOVE).
+decreaseTurn(FOOD, CLEAR, ENERGY, LOVE, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, NLIFE):-
+    NFOOD is FOOD - 5,
+    NCLEAR is CLEAR - 10,
+    NENERGY is ENERGY - 5,
+    NLOVE is LOVE - 10,
+    NLIFE is LIFE - 20,
+    criticalFood(NFOOD).
+decreaseTurn(FOOD, CLEAR, ENERGY, LOVE, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, NLIFE):-
+    NFOOD is FOOD - 5,
+    NCLEAR is CLEAR - 10,
+    NENERGY is ENERGY - 5,
+    NLOVE is LOVE - 10,
+    NLIFE is LIFE - 20,
+    criticalEnergy(NENERGY).
+decreaseTurn(FOOD, CLEAR, ENERGY, LOVE, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, NLIFE):-
+    NFOOD is FOOD - 5,
+    NCLEAR is CLEAR - 10,
+    NENERGY is ENERGY - 5,
+    NLOVE is LOVE - 10,
+    NLIFE is LIFE - 20,
+    criticalTrash(NCLEAR).
+decreaseTurn(FOOD, CLEAR, ENERGY, LOVE, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, LIFE):-
+    NFOOD is FOOD - 5,
+    NCLEAR is CLEAR - 10,
+    NENERGY is ENERGY - 5,
+    NLOVE is LOVE - 10,
+    valid(NFOOD, NCLEAR, NENERGY, NLOVE, LIFE),
+    not(critical(NLOVE, NFOOD, NENERGY, NCLEAR)).
 
-printInfo(CLEAR, FOOD, ENERGY, LOVE, LIFE):-
-    % writeln(NAME),
+%%%%% critical messages %%%%%
+critical(L, F, E, C):-
+  criticalLove(L),
+  criticalFood(F),
+  criticalEnergy(E),
+  criticalTrash(C).
+criticalLove(LOVE):-
+  LOVE =< 20,
+  writeln("CARÊNCIA CRÍTICA!! Parece que você é um verdadeiro BRUTA-MONTES, cuide do seu calango seu(a) cabra da peste!!").
+criticalFood(FOOD):-
+  FOOD =< 20,
+  writeln("PERIGO EMINENTE!!! Seu calango está a com fome alta, alimente-o já!").
+criticalEnergy(ENERGY):-
+  ENERGY =< 30,
+  writeln("SONO CRÍTICO!!! Seu calango precisa de um descanso ou não terá energia para escapar de predadores. Apague a luz!").
+criticalTrash(CLEAR):-
+  CLEAR =< 70,
+  writeln("PERIGO EMINENTE!!! Seu calango está a com fome alta, alimente-o já!").
+%%%%% critical messages %%%%%
+
+
+printInfo(NAME, FOOD, CLEAR, ENERGY, LOVE, LIFE):-
+    writeln(NAME),
     write("Comida: "),
     writeln(FOOD),
     write("Limpeza: "),
