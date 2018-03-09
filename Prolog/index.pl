e0dd8fafd9aa9357fe19d8b517c5bc1d69309d51
FelipeMarinho97 <felipevm97@gmail.com>
2018-03-09 11:22:38 -0300

    introdução de bug

--- Prolog/index.pl
+++ Prolog/index.pl
@@ -31,6 +31,7 @@ tableInstruction:-
 startGame:-
     writeln("Digite o nome do seu calango:"),
     read(NAME),
+    assert(nome(NAME)),
     mainCircle(NAME, 100, 100, 100, 100, 100, false).
 
 mainCircle(NAME, _, _, _, _, 0, _):- write(NAME), write(" morreu").
@@ -54,43 +55,24 @@ getAction(ACTION):-
     writeln("0- Matar calango"),
     read(ACTION).
 
-actionGame(FOOD, CLEAR, ENERGY, LOVE, _, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, NSLEEP, LIFE, 1):- % alimentar
-    checkUpperLimits(FOOD + 25, Z),
+actionGame(FOOD, CLEAR, ENERGY, LOVE, _, LIFE, NFOOD, CLEAR, ENERGY, NLOVE, false, LIFE, 1):- % alimentar
+    hasUpperLimits(FOOD + 25, Z),
     NFOOD is Z + 5,
-    NCLEAR is CLEAR,
-    NENERGY is ENERGY,
-    NLOVE is LOVE + 5,
-    NSLEEP=false.
-actionGame(FOOD, CLEAR, ENERGY, LOVE, _, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, NSLEEP, LIFE, 2):- % limpar
-    NFOOD is FOOD,
+    NLOVE is LOVE + 5.
+actionGame(FOOD, _, ENERGY, LOVE, _, LIFE, FOOD, NCLEAR, ENERGY, NLOVE, false, LIFE, 2):- % limpar
     NCLEAR is 110,
-    NENERGY is ENERGY,
-    NLOVE is LOVE + 5,
-    NSLEEP=false.
-actionGame(FOOD, CLEAR, ENERGY, LOVE, _, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, NSLEEP, NLIFE, 3):- % dormir
-    checkUpperLimits(ENERGY + 25, Z),
-    NFOOD is FOOD,
-    NCLEAR is CLEAR,
-    NENERGY is Z + 5,
-    NLOVE is LOVE,
-    NLIFE is LIFE + 2,
-    NSLEEP=true.
-actionGame(FOOD, CLEAR, ENERGY, LOVE, _, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, NSLEEP, LIFE, 4):- % dar carinho
-    checkUpperLimits(LOVE + 40, Z),
-    NFOOD is FOOD,
-    NCLEAR is CLEAR,
-    NENERGY is ENERGY,
-    NLOVE is Z + 10,
-    NSLEEP=false.
-actionGame(FOOD, CLEAR, ENERGY, LOVE, _, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, NSLEEP, NLIFE, 5):- % dar injecao
-    checkUpperLimits(LIFE + 50, Z),
-    NFOOD is FOOD,
-    NCLEAR is CLEAR,
-    NENERGY is ENERGY,
-    NLOVE is LOVE - 30,
-    NLIFE is Z + 20,
-    NSLEEP=false.
-actionGame(_, _, _, _, _, _, _, _, _, _, _, _, ACTION):- nome(NAME), mainCircle(JUAN, _, _, _, _, ACTION, _). % matar
+    NLOVE is LOVE + 5.
+actionGame(FOOD, CLEAR, ENERGY, LOVE, _, LIFE, FOOD, CLEAR, NENERGY, LOVE, true, ZLIFE, 3):- % dormir
+    hasUpperLimits(ENERGY + 25, Z),
+    hasUpperLimits(LIFE + 2, ZLIFE),
+    NENERGY is Z + 5.
+actionGame(FOOD, CLEAR, ENERGY, LOVE, _, LIFE, FOOD, CLEAR, ENERGY, NLOVE, false, LIFE, 4):- % dar carinho
+    hasUpperLimits(LOVE + 40, Z),
+    NLOVE is Z + 10.
+actionGame(FOOD, CLEAR, ENERGY, LOVE, _, LIFE, FOOD, CLEAR, ENERGY, NLOVE, false, NLIFE, 5):- % dar injecao
+    hasUpperLimits(LIFE + 50, NLIFE),
+    NLOVE is LOVE - 30.
+actionGame(_, _, _, _, _, _, _, _, _, _, _, _, ACTION):- nome(NAME), mainCircle(NAME, _, _, _, _, ACTION, _). % matar
 
 
 actionSleep(NAME, FOOD, CLEAR, ENERGY, LOVE, LIFE, SLEEP, 2):- % continuar dormindo
@@ -111,28 +93,23 @@ getActionSleep(ACTION):-
     writeln("2- Continuar dormindo"),
     read(ACTION).
 
-hasDiscounted(FOOD, CLEAR, ENERGY, LOVE, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, NLIFE):-
-    checkLowerLimits(FOOD - 5,ZFOOD),
-    checkLowerLimits(CLEAR - 10,ZCLEAR),
-    checkLowerLimits(ENERGY - 5,ZENERGY),
-    checkLowerLimits(LOVE - 10,ZLOVE),
-    checkLowerLimits(LIFE - 20,ZLIFE),
-    NFOOD is ZFOOD,
-    NCLEAR is ZCLEAR,
-    NENERGY is ZENERGY,
-    NLOVE is ZLOVE,
-    NLIFE is ZLIFE.
+hasDiscount(FOOD, CLEAR, ENERGY, LOVE, LIFE, ZFOOD, ZCLEAR, ZENERGY, ZLOVE, ZLIFE):-
+    hasLowerLimits(FOOD - 5, ZFOOD),
+    hasLowerLimits(CLEAR - 10, ZCLEAR),
+    hasLowerLimits(ENERGY - 5, ZENERGY),
+    hasLowerLimits(LOVE - 10, ZLOVE),
+    hasLowerLimits(LIFE - 20, ZLIFE).
 decreaseTurn(FOOD, CLEAR, ENERGY, LOVE, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, NLIFE):-
-    hasDiscounted(FOOD, CLEAR, ENERGY, LOVE, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, NLIFE),
+    hasDiscount(FOOD, CLEAR, ENERGY, LOVE, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, NLIFE),
     criticalLove(NLOVE).
 decreaseTurn(FOOD, CLEAR, ENERGY, LOVE, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, NLIFE):-
-    hasDiscounted(FOOD, CLEAR, ENERGY, LOVE, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, NLIFE),
+    hasDiscount(FOOD, CLEAR, ENERGY, LOVE, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, NLIFE),
     criticalFood(NFOOD).
 decreaseTurn(FOOD, CLEAR, ENERGY, LOVE, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, NLIFE):-
-    hasDiscounted(FOOD, CLEAR, ENERGY, LOVE, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, NLIFE),
+    hasDiscount(FOOD, CLEAR, ENERGY, LOVE, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, NLIFE),
     criticalEnergy(NENERGY).
 decreaseTurn(FOOD, CLEAR, ENERGY, LOVE, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, NLIFE):-
-    hasDiscounted(FOOD, CLEAR, ENERGY, LOVE, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, NLIFE),
+    hasDiscount(FOOD, CLEAR, ENERGY, LOVE, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, NLIFE),
     criticalTrash(NCLEAR).
 decreaseTurn(FOOD, CLEAR, ENERGY, LOVE, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, LIFE):-
     NFOOD is FOOD - 5,
@@ -141,16 +118,16 @@ decreaseTurn(FOOD, CLEAR, ENERGY, LOVE, LIFE, NFOOD, NCLEAR, NENERGY, NLOVE, LIF
     NLOVE is LOVE - 10,
     not(critical(NLOVE, NFOOD, NENERGY, NCLEAR)).
 
-checkUpperLimits(X,Z) :-
+hasUpperLimits(X,Z) :-
     (  X > 100
-    -> Z = 100
-    ;  Z = X
+    -> Z is 100
+    ;  Z is X
     ).
 
-checkLowerLimits(X,Z) :-
+hasLowerLimits(X,Z) :-
     (  X < 0
-    -> Z = 0
-    ;  Z = X
+    -> Z is 0
+    ;  Z is X
     ).
 
 %%%%% critical messages %%%%%
@@ -169,17 +146,20 @@ criticalEnergy(ENERGY):-
   ENERGY =< 30,
   writeln("SONO CRÍTICO!!! Seu calango precisa de um descanso ou não terá energia para escapar de predadores. Apague a luz!").
 criticalTrash(CLEAR):-
-  CLEAR =< 50,
+  CLEAR =< 40,
+  separator,
   writeln("QUE SUJEIRA!!! Seu calango está cambaleando no lixo!").
 %%%%% critical messages %%%%%
 
 
 printInfo(NAME, FOOD, CLEAR, ENERGY, LOVE, LIFE):-
+    separator,
     writeln(NAME),
     write("Comida: "), writeln(FOOD),
     write("Limpeza: "), writeln(CLEAR),
     write("Energia: "), writeln(ENERGY),
     write("Carinho: "), writeln(LOVE),
-    write("Vida: "), writeln(LIFE).
+    write("Vida: "), writeln(LIFE), nl.
 
 decreaseTurn:- write("").
+separator:- writeln("=================================================").
